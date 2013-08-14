1923fbc2d291f78baa43adfd7cd5372e965519ea
Cory Stephenson <aevin@me.com>
2013-08-14 15:32:52 -0500

    Add unicorn.

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "sidekiq"
 gem "simple_form"
 gem "turbolinks"
 gem "uglifier", ">= 1.3.0"
+gem "unicorn"
 gem "zurb-foundation"
 
 group :development do
---
+++ config/unicorn.rb
@@ -0,0 +1,69 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap "TERM" do
+    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
+    Process.kill "QUIT", Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+
+# Use at least one worker per core if you're on a dedicated server,
+# more will usually help for _short_ waits on databases/caches.
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 4)
+
+# Since Unicorn is never exposed to outside clients, it does not need to
+# run on the standard HTTP port (80), there is no reason to start Unicorn
+# as root unless it's from system init scripts.
+# If running the master process as root and the workers as an unprivileged
+# user, do this to switch euid/egid in the workers (also chowns logs):
+# user "unprivileged_user", "unprivileged_group"
+
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+APP_PATH="/var/www/myweighttracker"
+working_directory APP_PATH # available in 0.94.0+
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen APP_PATH + "/tmp/sockets/unicorn.sock", :backlog => 64
+listen 8080, :tcp_nopush => true
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+pid APP_PATH + "/tmp/pid/unicorn.pid"
+
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
